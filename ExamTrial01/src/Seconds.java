d5c6493f65ec0d3383c30a4223796915b250f9b3
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-10 09:32:26 +0200

    Trial 1 trying to print out the numbers

--- ExamTrial01/src/Seconds.java
+++ ExamTrial01/src/Seconds.java
@@ -1,19 +1,22 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 // Create a function that takes a list as a parameter,
 // and returns a new list with every second element from the orignal list
 // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
 public class Seconds {
   public static void main(String[] args) {
-
+    int [] mylist = {};
+    System.out.println(mylist.listChan ger);
 
   }
 
-  public void listChanger(int [] list){
+  public int[] listChanger(int [] list){
     int [] numList = {1,2,3,4,5};
     int [] readylist = {};
     for (int i = 0; i < numList.length; i++){
-      
+      System.out.println(readylist[i + 1]);
     }
+    return readylist;
   }
 }
