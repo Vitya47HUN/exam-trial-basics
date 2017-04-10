71852f6d7a12d3aed1ec50692b9769e7a1f08b34
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-10 12:40:02 +0200

    Trial 4(cuboid) deleted unneccesary whitespaces, comments, indentation

--- ExamTrial01/src/Cuboid.java
+++ ExamTrial01/src/Cuboid.java
@@ -1,15 +1,8 @@
-// Create a class that represents a cuboid:
-// It should take its three dimensions as constructor parameters (numbers)
-// It should have a method called `getSurface` that returns the cuboid's surface
-// It should have a method called `getVolume` that returns the cuboid's volume
-
 public class Cuboid {
-
   static class Numbers {
     int length;
     int height;
     int breadth;
-
 
     Numbers(int length, int height, int breadth) {
       this.length = length;
@@ -18,7 +11,6 @@ public class Cuboid {
     }
   }
   public static void main(String[] args) {
-
     System.out.println("The surface of the cuboid: " + getSurface(new Numbers(1,2,3)));
     System.out.println("The volume of the cuboid: " + getVolume(new Numbers(1,2,3)));
   }
