2419829f6a9ddaab8ceb0e9a3d950d7f59d86cf2
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-10 10:18:29 +0200

    Trial 3 created a function but its not working yet

--- ExamTrial01/src/PirateCounter.java
+++ ExamTrial01/src/PirateCounter.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class PirateCounter{
@@ -29,5 +30,11 @@ public class PirateCounter{
     // And returns a list of names containing the pirates that
     // - have wooden leg and
     // - have more than 15 gold
+  }
+  public static ArrayList<Pirate> listSorter(List<Pirate> list) {
+    ArrayList<Pirate> sortedList = new ArrayList<>();
+    for (int i = 0; i < list.size();i++){
+      if (list.)
+    }
   }
 }
