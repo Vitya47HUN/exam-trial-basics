f1c394e2d88297e5e8a95088652d58682d97de0f
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-10 12:37:32 +0200

    Trial 4 , got function that returns cuboid volume and uses the constructors variables

--- ExamTrial01/src/Cuboid.java
+++ ExamTrial01/src/Cuboid.java
@@ -19,15 +19,15 @@ public class Cuboid {
   }
   public static void main(String[] args) {
 
-    System.out.println(getSurface(new Numbers(1,2,3)));
+    System.out.println("The surface of the cuboid: " + getSurface(new Numbers(1,2,3)));
+    System.out.println("The volume of the cuboid: " + getVolume(new Numbers(1,2,3)));
   }
-
   public static int getSurface(Numbers nums){
     int surface = 2 * ((nums.length * nums.breadth) + (nums.breadth * nums.height) + (nums.height * nums.length));
     return surface;
   }
-//  public static int getVolume(Numbers nums){
-//    int volume =
-//    return volume;
-//  }
+  public static int getVolume(Numbers nums){
+    int volume = nums.length * nums.breadth * nums.height;
+    return volume;
+  }
 }
