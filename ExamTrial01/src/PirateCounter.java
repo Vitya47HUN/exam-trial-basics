88149e994dfed7c260061abaddc1ed9c9dd6e367
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-10 10:25:28 +0200

    Trial 3 function tries to store information in a sorted list , but its not printing out anything

--- ExamTrial01/src/PirateCounter.java
+++ ExamTrial01/src/PirateCounter.java
@@ -30,11 +30,18 @@ public class PirateCounter{
     // And returns a list of names containing the pirates that
     // - have wooden leg and
     // - have more than 15 gold
+
+    listSorter(pirates);
   }
   public static ArrayList<Pirate> listSorter(List<Pirate> list) {
     ArrayList<Pirate> sortedList = new ArrayList<>();
     for (int i = 0; i < list.size();i++){
-      if (list.)
+      for (int j = 0; j < list.indexOf(i); j++)
+
+      if (list.indexOf(j) < 15){
+        sortedList.add(list.get(j));
+      }
     }
+    return sortedList;
   }
 }
