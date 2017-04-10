08a440f2bd6d9d8d4476dc1ee0773e39d0f18022
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-10 09:43:12 +0200

    Trial 1 printed out good numbers but also with a lot of 0 index

--- ExamTrial01/src/Seconds.java
+++ ExamTrial01/src/Seconds.java
@@ -1,21 +1,30 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 // Create a function that takes a list as a parameter,
 // and returns a new list with every second element from the orignal list
 // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
 public class Seconds {
   public static void main(String[] args) {
-    int [] mylist = {};
-    System.out.println(mylist.listChan ger);
+    List<Integer> mylist = new ArrayList<Integer>();
+    mylist.add(1);
+    mylist.add(2);
+    mylist.add(3);
+    mylist.add(4);
+    mylist.add(5);
+    System.out.println(listChanger(mylist));
 
   }
 
-  public int[] listChanger(int [] list){
-    int [] numList = {1,2,3,4,5};
-    int [] readylist = {};
-    for (int i = 0; i < numList.length; i++){
-      System.out.println(readylist[i + 1]);
+  public static List<Integer> listChanger(List<Integer> list){
+    List<Integer> readylist = new ArrayList<Integer>();
+
+    for (int i = 0; i < list.size(); i++){
+      if (list.indexOf(i) % 2 != 0) {
+        readylist.add(i);
+
+      }
     }
     return readylist;
   }
