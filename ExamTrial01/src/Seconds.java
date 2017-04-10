c9b550ffc8bc9ce2e235929d19017375e2361f56
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-10 09:28:01 +0200

    Trial 1 started, making a functon wich changes the number of the lists

---
+++ ExamTrial01/src/Seconds.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+
+// Create a function that takes a list as a parameter,
+// and returns a new list with every second element from the orignal list
+// example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
+public class Seconds {
+  public static void main(String[] args) {
+
+
+  }
+
+  public void listChanger(int [] list){
+    int [] numList = {1,2,3,4,5};
+    int [] readylist = {};
+    for (int i = 0; i < numList.length; i++){
+      
+    }
+  }
+}
