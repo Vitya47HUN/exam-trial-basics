6e33897d9ad2bb082a32dd3db2cba6964acfe94b
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-10 12:20:20 +0200

    Trial 4 , got function that returns cuboid surface cant get the constructor working

--- ExamTrial01/src/Cuboid.java
+++ ExamTrial01/src/Cuboid.java
@@ -4,8 +4,26 @@
 // It should have a method called `getVolume` that returns the cuboid's volume
 
 public class Cuboid {
+
+  static class Numbers {
+    int length;
+    int height;
+    int breadth;
+
+
+    Numbers(int length, int height, int breadth) {
+      this.length = length;
+      this.height = height;
+      this.breadth = breadth;
+    }
+  }
   public static void main(String[] args) {
 
+    System.out.println(getSurface(Numbers));
+  }
 
+  public static int getSurface(int l, int h, int b){
+    int surface = 2 * ((l * b) + (b * h) + (h * l));
+    return surface;
   }
 }
